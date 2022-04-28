03539c71ad4e32e423dbee39febd97a7dad4342f
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-04-28 19:06:07 -0300

    Task 2 - Scanning Manual

--- src/rpn/Stacker.java
+++ src/rpn/Stacker.java
@@ -14,55 +14,72 @@ public class Stacker {
         Stack<Double> stack = new Stack<Double>();
 
         ArrayList<Token> scanning = new ArrayList<>();
+        boolean error = false;
 
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
             String value = bufferedReader.readLine();
-            while (value != null) {
-                if(value.matches("[0-9]*") || value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")){
-                    if(!value.equals("+") && !value.equals("-") && !value.equals("*") && !value.equals("/")){
+
+            while(value != null) {
+                // [Scanning manual] Pra não usar o "loop for" é possível usar expressões regulares, mas não é esse o objetivo da task2
+                for (int i = 0; i < value.length(); i++) {
+                    if (value.charAt(i) != '0' && value.charAt(i) != '1' && value.charAt(i) != '2' && value.charAt(i) != '3'
+                            && value.charAt(i) != '4' && value.charAt(i) != '5' && value.charAt(i) != '6' && value.charAt(i) != '7'
+                            && value.charAt(i) != '8' && value.charAt(i) != '9' && value.charAt(i) != '+' && value.charAt(i) != '-'
+                            && value.charAt(i) != '*' && value.charAt(i) != '/') {
+
+                        error = true;
+                        break;
+                    }
+                }
+
+                if (error) {  // Pra não fazer dessa maneira, eu poderia adicionar um try/catch, mas pra essa entrega deve ser suficiente essa maneira
+                    scanning.clear(); // Limpa qualquer entrada anterior que tenha sido escaneada corretamente
+                    stack.clear(); // Limpa qualquer entrada anterior válida da pilha
+
+                    System.out.println("Error: Unexpected character: " + value);
+                    break;
+                } else {
+                    if (!value.equals("+") && !value.equals("-") && !value.equals("*") && !value.equals("/")) {
                         stack.push(Double.parseDouble(value));
                         scanning.add(new Token(TokenType.NUM, value));
                     }
-                    else{
+                    else {
                         double v2 = stack.pop();
                         double v1 = stack.pop();
 
-                        if(value.equals("+")){
+                        if (value.equals("+")) {
                             scanning.add(new Token(TokenType.PLUS, value));
                             double newValue = v1 + v2;
                             stack.push(newValue);
                         }
-                        if(value.equals("-")){
+                        if (value.equals("-")) {
                             scanning.add(new Token(TokenType.MINUS, value));
                             double newValue = v1 - v2;
                             stack.push(newValue);
                         }
-                        if(value.equals("*")){
+                        if (value.equals("*")) {
                             scanning.add(new Token(TokenType.STAR, value));
                             double newValue = v1 * v2;
                             stack.push(newValue);
                         }
-                        if(value.equals("/")){
+                        if (value.equals("/")) {
                             scanning.add(new Token(TokenType.SLASH, value));
                             double newValue = v1 / v2;
                             stack.push(newValue);
                         }
                     }
-                    value = bufferedReader.readLine();
                 }
-                else{
-                    System.out.println("Error: Unexpected character: " + value);
-                    scanning.clear(); // Impede de listar qualquer token já escaneado
-                    stack.clear(); // limpa qualquer valor que esteja na pilha
-                    break;
-                }
+                value = bufferedReader.readLine();
             }
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
-        for (int i = 0; i < scanning.size(); i++) {
-            System.out.println(scanning.get(i));
+
+        if(scanning.size() > 0){
+            for (Token token : scanning) {
+                System.out.println(token);
+            }
+            System.out.printf("\nSaída: %.2f\n", stack.pop());
         }
-        if(stack.size() > 0) System.out.printf("%.2f\n", stack.pop());
     }
 }
